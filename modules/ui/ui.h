6682e2ed26bc4d9637d92fcb35d2a46b565b1335
Argon <31993671+argongs@users.noreply.github.com>
2022-08-17 12:32:06 +0000

    added a enum to represent the yes/no response from the end user and updated the name of a function

--- modules/ui/ui.h
+++ modules/ui/ui.h
@@ -1,11 +1,13 @@
 // Provide a prototype of all functions that will be used for user interaction
 
+typedef enum {
+    YES = 'y',
+    NO = 'n'
+}user_binary_response_enum;
+
 void show_grid(grid_struct grid);
 void get_input_for_grid(player_struct player, grid_struct* grid); // take input iff the grid is not in win status
-void ask_for_rematch(player_struct player, player_struct opponent);
+void is_player_interested_in_a_rematch();
 
 void show_opponent_request(player_struct* opponent);
 bool accept_opponent_request(player_struct player, player_struct opponent);
-
-bool validate_command_line_input(char* name, game_character character);
-bool validate_grid_input(grid_struct grid);
