9208cc870bb934d42b49bd9a21eadbe63a8388c8
Argon <31993671+argongs@users.noreply.github.com>
2022-08-18 15:49:33 +0000

    updated grid input function in order to allow appropriate response w.r.t input and fixed the return type of rematch input function

--- modules/ui/ui.h
+++ modules/ui/ui.h
@@ -6,8 +6,8 @@ typedef enum {
 }user_binary_response_enum;
 
 void show_grid(grid_struct grid);
-void get_input_for_grid(player_struct player, grid_struct* grid); // take input iff the grid is not in win status
-void is_player_interested_in_a_rematch();
+grid_status_enum get_input_for_grid(player_struct player, grid_struct* grid); // take input iff the grid is not in win status
+bool is_player_interested_in_a_rematch();
 
 void show_opponent_request(player_struct opponent);
 bool accept_opponent_request(player_struct player, player_struct opponent);
